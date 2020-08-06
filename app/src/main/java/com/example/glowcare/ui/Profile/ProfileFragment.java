6ed6db549b0ac806929637b9bb26fc7efa808c59
Rathan Sreeramoju <Rathan Sreeramoju>
2020-08-05 22:13:58 -0400

    Edited fragments

--- app/src/main/java/com/example/glowcare/ui/Profile/ProfileFragment.java
+++ app/src/main/java/com/example/glowcare/ui/Profile/ProfileFragment.java
@@ -1,27 +1,23 @@
-package com.example.glowcare.ui.notifications;
+package com.example.glowcare.ui.Profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.glowcare.R;
 
-public class NotificationsFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
 
-View view;
+    View view;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-        view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
         return view;
     }
 }
--- app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
@@ -1,9 +1,11 @@
 package com.example.glowcare.ui.home;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,20 +18,15 @@ import com.example.glowcare.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    View view;
+    ImageView imageView;
+    TextView text1 ,text2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+
     }
+
 }
