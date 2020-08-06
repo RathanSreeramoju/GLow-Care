7e0bb58a5bca966da1be44910c9166fc7244314d
Manoj496 <manoj.legendry@gmail.com>
2020-08-06 03:41:04 -0400

    View Product

--- app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.glowcare.ui.home;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,9 +15,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.glowcare.ProductView;
 import com.example.glowcare.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment{
 
     View view;
     ImageView imageView;
@@ -25,8 +27,17 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_home, container, false);
+        ImageView img1=(ImageView) view.findViewById(R.id.img1);
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeFragment.this.getActivity(), ProductView.class));
+            }
+        });
         return view;
 
+
     }
+
 
 }
