e051262b918051b84a4a3cea11dc7a1c8b601d74
Rathan Sreeramoju <Rathan Sreeramoju>
2020-08-06 10:22:38 -0400

    Settings Fragment Updated

--- app/src/main/java/com/example/glowcare/ui/Settings/SettingsFargment.java
+++ app/src/main/java/com/example/glowcare/ui/Settings/SettingsFargment.java
@@ -1,4 +1,22 @@
 package com.example.glowcare.ui.Settings;
 
-public class SettingsFargment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.glowcare.R;
+
+public class SettingsFargment extends Fragment {
+
+    View view;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.activity_settings, container, false);
+        return view;
+    }
 }
