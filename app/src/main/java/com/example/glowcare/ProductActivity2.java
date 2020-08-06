42052ec028de90eb2acefe30f5e7fcdb7ca3146d
Manoj496 <manoj.legendry@gmail.com>
2020-08-06 04:20:21 -0400

    Product View2

---
+++ app/src/main/java/com/example/glowcare/ProductActivity2.java
@@ -0,0 +1,26 @@
+package com.example.glowcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.glowcare.ui.home.HomeFragment;
+
+public class ProductActivity2 extends AppCompatActivity {
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product2);
+        button=(Button)findViewById(R.id.back2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ProductActivity2.this, HomeFragment.class));
+            }
+        });
+    }
+}
