ca75235484f51d2e95fb4ce493388df0a452df29
Ujwal Seethamraju <Ujwal Seethamraju>
2020-09-02 19:19:46 -0400

    Splash Screen Finished

---
+++ app/src/main/java/com/example/glowcare/Handler/MyApp.java
@@ -0,0 +1,26 @@
+package com.example.glowcare.Handler;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+    private static MyApp mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance=this;
+        System.out.println("my app___"+mInstance);
+    }
+
+    public static synchronized MyApp getInstance() {
+        System.out.println("instance__"+new MyApp());
+        return mInstance;
+    }
+
+    public void setConnectivityListener(ConnectivityReceiver.connectivityReceiverListener listener){
+//        Toast.makeText(mInstance, "when offline to online", Toast.LENGTH_SHORT).show();
+        ConnectivityReceiver.connectivityReceiverListener=listener;
+
+
+    }
+}
---
+++ app/src/main/java/com/example/glowcare/SplashScreenActivity.java
@@ -0,0 +1,72 @@
+package com.example.glowcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.glowcare.Handler.ConnectivityReceiver;
+import com.example.glowcare.Handler.MyApp;
+
+public class SplashScreenActivity extends AppCompatActivity{
+
+    private ImageView home;
+    private TextView name;
+    private Animation top,bottom,expected_anim;
+    private Context context;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        context = getApplicationContext();
+
+        init();
+
+
+
+    }
+
+
+    private void init() {
+
+        home = findViewById(R.id.imageView4);
+        name = findViewById(R.id.name);
+
+        //for animation initialization
+        top = AnimationUtils.loadAnimation(context,R.anim.top_anim);
+        bottom = AnimationUtils.loadAnimation(context,R.anim.bottom_anim);
+        expected_anim = AnimationUtils.loadAnimation(context,R.anim.current_position_to_expected_position);
+
+        //setAnim
+
+        home.startAnimation(top);
+        name.startAnimation(bottom);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            /**
+             * This method allows to open the splashcreen activity runs for particular amount of time.
+             */
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
+
+    }
+
+}
