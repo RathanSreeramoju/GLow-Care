3f2eae696e720ddecf59f4b6c5f3e61841c4fdb3
sai praneeth <inupamulapraneeth@gmail.com>
2020-09-04 12:46:58 -0400

    HomeFragment FInal edit done

--- app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.glowcare.ui.home;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,7 +10,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 import com.example.glowcare.ProductActivity1;
@@ -17,9 +21,56 @@ import com.example.glowcare.ProductActivity2;
 import com.example.glowcare.ProductActivity3;
 import com.example.glowcare.ProductActivity4;
 import com.example.glowcare.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class HomeFragment extends Fragment{
 
+    View view;
+    private RecyclerView recyclerView;
+    private FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();;
+    private  HomeAdapter adapter;
+    private CollectionReference notebookRef = firebaseFirestore.collection("Products");
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+        //Query
+        Query query = notebookRef.orderBy("name", Query.Direction.ASCENDING);
+
+
+
+        //RecyclerOptions
+        FirestoreRecyclerOptions<HomeModel> options = new FirestoreRecyclerOptions.Builder<HomeModel>()
+                .setQuery(query,HomeModel.class)
+                .build();
+        adapter = new HomeAdapter(options);
+        recyclerView = view.findViewById(R.id.firestore_list);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(adapter);
+        return view;
+
+
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
 
 
