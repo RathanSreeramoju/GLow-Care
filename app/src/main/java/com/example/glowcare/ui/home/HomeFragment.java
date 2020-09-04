92d5b372055b79f44b64c6b46f0a676d0270b6bb
sai praneeth <inupamulapraneeth@gmail.com>
2020-09-04 12:44:37 -0400

    HomeFragment

--- app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/glowcare/ui/home/HomeFragment.java
@@ -20,43 +20,6 @@ import com.example.glowcare.R;
 
 public class HomeFragment extends Fragment{
 
-    View view;
-    TextView text1 ,text2;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_home, container, false);
-        ImageView img1=(ImageView) view.findViewById(R.id.img1);
-        img1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeFragment.this.getActivity(), ProductActivity1.class));
-            }
-        });
-        ImageView img2=(ImageView) view.findViewById(R.id.img2);
-        img2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeFragment.this.getActivity(), ProductActivity2.class));
-            }
-        });
-        ImageView img3=(ImageView) view.findViewById(R.id.img3);
-        img3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeFragment.this.getActivity(), ProductActivity3.class));
-            }
-        });
-        ImageView img4=(ImageView) view.findViewById(R.id.img4);
-        img4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeFragment.this.getActivity(), ProductActivity4.class));
-            }
-        });
-        return view;
-
-
     }
 
 
