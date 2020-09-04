3c5f2eea83b2b6255f0411c5b0c8919abc15927d
sai praneeth <inupamulapraneeth@gmail.com>
2020-09-04 12:14:51 -0400

    Created Home Adapter

---
+++ app/src/main/java/com/example/glowcare/ui/home/HomeAdapter.java
@@ -0,0 +1,61 @@
+package com.example.glowcare.ui.home;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.glowcare.R;
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+import java.util.ArrayList;
+
+public class HomeAdapter extends FirestoreRecyclerAdapter<HomeModel,HomeAdapter.HomeViewHolder> {
+
+    ArrayList<HomeModel> products = new ArrayList<>();
+    private Context context;
+    /**
+     * Create a new RecyclerView adapter that listens to a Firestore Query.  See {@link
+     * FirestoreRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public HomeAdapter(@NonNull FirestoreRecyclerOptions<HomeModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull HomeViewHolder holder, int position, @NonNull HomeModel model) {
+        holder.listName.setText(model.getName());
+        holder.listPrice.setText(model.getPrice()+"");
+        // Glide.with(context).load(model.getImageurl()).into(holder.image);
+    }
+
+    @NonNull
+    @Override
+    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view1 = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items_home,parent,false);
+
+        return new HomeViewHolder(view1);
+    }
+
+    public class HomeViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView listName,listPrice;
+        private ImageView image;
+        public HomeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            listName = itemView.findViewById(R. id.list_name);
+            listPrice = itemView.findViewById(R.id.list_price);
+            // image = itemView.findViewById(R.id.imageView8);
+        }
+    }
+}
