2629938eba21919a308c2824df24ed355e120768
Manoj496 <manoj.legendry@gmail.com>
2020-09-05 00:17:18 -0400

    Added code to retrieve data from firestore and display on CartFragment

--- app/src/main/java/com/example/glowcare/ui/cart/CartFragment.java
+++ app/src/main/java/com/example/glowcare/ui/cart/CartFragment.java
@@ -6,34 +6,75 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.glowcare.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class CartFragment extends Fragment {
-
-
-View view;
-Button button1;
-private Context context;
+    private FirebaseFirestore db;
+    private CartAdapter adapter;
+    private RecyclerView recyclerView;
+    ArrayList<CartModel> cartList;
+    TextView buyNow;
+    CartModel cart;
+    private Context context;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        view = inflater.inflate(R.layout.fragment_cart, container, false);
-        //button1 = (Button) view.findViewById(R.id.order);
-        context = getContext();
-       /* button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "Order Placed", Toast.LENGTH_SHORT).show();
+        return inflater.inflate(R.layout.fragment_cart, container, false);
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        db = FirebaseFirestore.getInstance();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView = view.findViewById(R.id.cartRecycle);
+        recyclerView.setLayoutManager(layoutManager);
+        cartList = new ArrayList<>();
+        buyNow = view.findViewById(R.id.btnBuyNow);
+
+        db.collection("Cart").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    for(DocumentSnapshot snapshot:task.getResult()){
+                        cart = new CartModel(snapshot.getId(),
+                                             snapshot.getString("name"),
+                                             snapshot.getDouble("price"),
+                                             snapshot.getString("imageurl"));
+                        cartList.add(cart);
+                    }
+                    adapter = new CartAdapter(cartList,getActivity().getApplicationContext());
+                    recyclerView.setAdapter(adapter);
+
+                    adapter.setOnItemClickListener(new CartAdapter.OnDeleteListener() {
+                        @Override
+                        public void onDeleteClick(int position) {
+                            db.collection("CartList").document(cartList.get(position).getId()).delete();
+                        }
+                    });
+
+                }
+                else {
+                    Toast.makeText(getActivity().getApplicationContext(),task.getException()+"", Toast.LENGTH_LONG).show();
+                }
             }
         });
-*/
-        return view;
-
     }
 }
--- app/src/main/java/com/example/glowcare/ui/cart/CartModel.java
+++ app/src/main/java/com/example/glowcare/ui/cart/CartModel.java
@@ -1,9 +1,18 @@
 package com.example.glowcare.ui.cart;
 
 public class CartModel {
+    private String id;
     private String name;
     private double price;
     private String imageurl;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public  String getImageurl() {
         return imageurl;
@@ -28,11 +37,12 @@ public class CartModel {
         return price;
     }
 
-    public void setPrice(long price) {
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public CartModel(String name, double price,String imageurl) {
+    public CartModel(String id, String name, double price, String imageurl) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.imageurl = imageurl;
